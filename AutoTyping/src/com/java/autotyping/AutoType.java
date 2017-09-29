9db6738d8ab83fed74510adbe2882e62796e7034
Rino Wilson C <rino1000@gmail.com>
2017-09-29 20:58:19 +0530

    fixed gitignore

---
+++ AutoTyping/src/com/java/autotyping/AutoType.java
@@ -0,0 +1,51 @@
+package com.java.autotyping;
+
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+
+public class AutoType {
+	public static void main(String[] args) {
+
+		try {
+			Runtime.getRuntime().exec("notepad.exe");
+			Thread.sleep(2000);
+			Robot r = new Robot();
+			r.keyPress(KeyEvent.VK_T);
+			Thread.sleep(500);
+			r.keyPress(KeyEvent.VK_H);
+			Thread.sleep(500);
+			r.keyPress(KeyEvent.VK_I);
+			Thread.sleep(500);
+			r.keyPress(KeyEvent.VK_S);
+			Thread.sleep(500);
+			r.keyPress(KeyEvent.VK_SPACE);
+			Thread.sleep(500);
+			r.keyPress(KeyEvent.VK_I);
+			Thread.sleep(500);
+			r.keyPress(KeyEvent.VK_S);
+			Thread.sleep(500);
+			r.keyPress(KeyEvent.VK_SPACE);
+			Thread.sleep(500);
+			r.keyPress(KeyEvent.VK_F);
+			Thread.sleep(500);
+			r.keyPress(KeyEvent.VK_U);
+			Thread.sleep(500);
+			r.keyPress(KeyEvent.VK_C);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// extra comments added
+
+	}
+}
